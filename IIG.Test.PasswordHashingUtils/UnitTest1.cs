3d89f9fa68741f0087f3fa40aea24bbb01116fb0
SunRiseGG <SunRiseGC4@gmail.com>
2020-12-12 16:03:08 +0200

    lab3

--- IIG.Test.PasswordHashingUtils/UnitTest1.cs
+++ IIG.Test.PasswordHashingUtils/UnitTest1.cs
@@ -8,242 +8,102 @@ namespace IIG.Test.PasswordHashingUtils
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCase_GetHash_0_1_2_4_5()
         {
             string password = "password42069";
             Assert.IsNotNull(PasswordHasher.GetHash(password));
-        }
-
-        [TestMethod]
-        public void TestMethod2()
-        {
-            string password = "";
+            password = "";
             Assert.IsNotNull(PasswordHasher.GetHash(password));
-        }
-
-        [TestMethod]
-        public void TestMethod3()
-        {
-            string password = " ";
+            password = " ";
             Assert.IsNotNull(PasswordHasher.GetHash(password));
+            password = "password42069";
+            Assert.IsNotNull(PasswordHasher.GetHash(password, "", 42069));
+            password = "password42069";
+            Assert.IsNotNull(PasswordHasher.GetHash(password, "nice"));
+            password = "password42069";
+            Assert.IsNotNull(PasswordHasher.GetHash(password, "nice", 0));
+            password = "password42069";
+            Assert.IsNotNull(PasswordHasher.GetHash(password, " ", 69));
+            password = "password42069";
+            Assert.AreEqual(PasswordHasher.GetHash(password, "nice", 69).Length, 64);
+
         }
 
         [TestMethod]
-        public void TestMethod4()
+        public void TestCase_GetHash_0_1_2_5()
         {
             string password = null;
             Assert.ThrowsException<System.ArgumentNullException>(() => PasswordHasher.GetHash(password));
         }
 
-
-
-
-
         [TestMethod]
-        public void TestMethod5()
+        public void TestCase_GetHash_0_1_2_3_4_5()
         {
-            string password = "password42069";
-            Assert.AreEqual(PasswordHasher.GetHash(password), PasswordHasher.GetHash(password));
+            string password = "-42069";
+            Assert.IsNotNull(PasswordHasher.GetHash(password));
+            Assert.AreEqual(PasswordHasher.GetHash(password).Length, 64);
         }
 
         [TestMethod]
-        public void TestMethod6()
-        {
-            string password1 = "password42069";
-            string password2 = "password42069";
-            Assert.AreEqual(PasswordHasher.GetHash(password1), PasswordHasher.GetHash(password2));
-        }
-
-        [TestMethod]
-        public void TestMethod7()
-        {
-            string password1 = "password4206969";
-            string password2 = "password42069";
-            Assert.AreNotEqual(PasswordHasher.GetHash(password1), PasswordHasher.GetHash(password2));
-        }
-
-        [TestMethod]
-        public void TestMethod8()
-        {
-            string password1 = "password4206969";
-            string password2 = "password42069";
-            Assert.AreNotEqual(PasswordHasher.GetHash(password1), PasswordHasher.GetHash(password2));
-        }
-
-        [TestMethod]
-        public void TestMethod9()
-        {
-            string password1 = "password42069";
-            string password2 = "PASSWORD42069";
-            Assert.AreNotEqual(PasswordHasher.GetHash(password1), PasswordHasher.GetHash(password2));
-        }
-
-        [TestMethod]
-        public void TestMethod10()
-        {
-            string password = "password42069";
-            Assert.IsNotNull(PasswordHasher.GetHash(password, null));
-        }
-
-        [TestMethod]
-        public void TestMethod11()
-        {
-            string password = "password42069";
-            Assert.IsNotNull(PasswordHasher.GetHash(password, null, 42069));
-        }
-
-        [TestMethod]
-        public void TestMethod12()
-        {
-            string password = "password42069";
-            Assert.IsNotNull(PasswordHasher.GetHash(password, "", 42069));
-        }
-
-        [TestMethod]
-        public void TestMethod13()
+        public void TestCase_Init_0_1_2_3()
         {
             string password = "password42069";
             Assert.AreEqual(PasswordHasher.GetHash(password, "", 42069), PasswordHasher.GetHash(password, "", 42069));
-        }
-
-        [TestMethod]
-        public void TestMethod14()
-        {
             string password1 = "password42069";
             string password2 = "password42069";
             Assert.AreEqual(PasswordHasher.GetHash(password1, "", 42069), PasswordHasher.GetHash(password2, "", 42069));
-        }
-
-        [TestMethod]
-        public void TestMethod15()
-        {
-            string password1 = "password42069";
-            string password2 = "password4206969";
+            password1 = "password42069";
+            password2 = "password4206969";
             Assert.AreNotEqual(PasswordHasher.GetHash(password1, "", 42069), PasswordHasher.GetHash(password2, "", 42069));
-        }
-
-        [TestMethod]
-        public void TestMethod16()
-        {
-            string password = "password42069";
+            password = "password42069";
             Assert.AreNotEqual(PasswordHasher.GetHash(password, "", 42069), PasswordHasher.GetHash(password, "", 69));
+            password1 = "password42069";
+            password2 = "password4206969";
+            Assert.AreEqual(PasswordHasher.GetHash(password1, "nice", 69).Length, PasswordHasher.GetHash(password2, "nice", 69).Length);
+            password = "password42069";
+            Assert.AreEqual(PasswordHasher.GetHash(password, "nice", 69).Length, PasswordHasher.GetHash(password, "nice42069", 69).Length);
+
         }
 
         [TestMethod]
-        public void TestMethod17()
-        {
-            string password1 = "password42069";
-            string password2 = "password42069";
-            Assert.AreNotEqual(PasswordHasher.GetHash(password1, "", 42069), PasswordHasher.GetHash(password2, "", 42069));
-        }
-
-        [TestMethod]
-        public void TestMethod18()
-        {
-            string password = "password42069";
-            Assert.IsNotNull(PasswordHasher.GetHash(password, "nice"));
-        }
-
-        [TestMethod]
-        public void TestMethod19()
-        {
-            string password = "password42069";
-            Assert.IsNotNull(PasswordHasher.GetHash(password, "nice"));
-        }
-
-        [TestMethod]
-        public void TestMethod20()
+        public void TestCase_Init_0_1_3()
         {
             string password = "password42069";
             Assert.AreEqual(PasswordHasher.GetHash(password, "nice"), PasswordHasher.GetHash(password, "nice"));
-        }
-
-        [TestMethod]
-        public void TestMethod21()
-        {
-            string password = "password42069";
+            password = "password42069";
             Assert.AreNotEqual(PasswordHasher.GetHash(password, "nice"), PasswordHasher.GetHash(password, "nice69"));
-        }
-
-        [TestMethod]
-        public void TestMethod22()
-        {
-            string password = "password42069";
+            password = "password42069";
             Assert.AreNotEqual(PasswordHasher.GetHash(password, "nice"), PasswordHasher.GetHash(password, "nice "));
-        }
-
-        [TestMethod]
-        public void TestMethod23()
-        {
-            string password = "password42069";
-            Assert.IsNotNull(PasswordHasher.GetHash(password, "nice", 0));
-        }
-
-        [TestMethod]
-        public void TestMethod24()
-        {
-            string password = "password42069";
-            Assert.IsNotNull(PasswordHasher.GetHash(password, " ", 69));
-        }
-
-        [TestMethod]
-        public void TestMethod25()
-        {
-            string password = "password42069";
-            Assert.AreEqual(PasswordHasher.GetHash(password, "nice", 69).Length, 64);
-        }
-
-        [TestMethod]
-        public void TestMethod26()
-        {
-            string password1 = "password42069";
-            string password2 = "password4206969";
-            Assert.AreEqual(PasswordHasher.GetHash(password1, "nice", 69).Length, PasswordHasher.GetHash(password2, "nice", 69).Length);
-        }
-
-        [TestMethod]
-        public void TestMethod27()
-        {
-            string password = "password42069";
-            Assert.AreEqual(PasswordHasher.GetHash(password, "nice", 69).Length, PasswordHasher.GetHash(password, "nice42069", 69).Length);
-        }
-
-        [TestMethod]
-        public void TestMethod28()
-        {
-            string password = "password42069";
-            PasswordHasher.Init("nice", 69);
+            password = "password42069";
+            PasswordHasher.Init("nice", 0);
             Assert.IsNotNull(PasswordHasher.GetHash(password));
-        }
-
-        [TestMethod]
-        public void TestMethod29()
-        {
-            string password = "password42069";
-            PasswordHasher.Init(null, 0);
-            Assert.IsNotNull(PasswordHasher.GetHash(password));
-        }
-
-        [TestMethod]
-        public void TestMethod30()
-        {
-            string password = "password42069";
-            PasswordHasher.Init("nice", 69);
+            password = "password42069";
+            PasswordHasher.Init("nice", 0);
             string hash1 = PasswordHasher.GetHash(password);
-            string hash2 = PasswordHasher.GetHash(password, "nice", 69);
+            string hash2 = PasswordHasher.GetHash(password, "nice", 0);
             Assert.AreEqual(hash1, hash2);
+            password = "password42069";
+            PasswordHasher.Init("nice", 0);
+            hash1 = PasswordHasher.GetHash(password);
+            hash2 = PasswordHasher.GetHash(password, "nice69", 0);
+            Assert.AreNotEqual(hash1, hash2);
+
         }
 
         [TestMethod]
-        public void TestMethod31()
+        public void TestCase_Init_0_2_3()
         {
             string password = "password42069";
-            PasswordHasher.Init("nice", 69);
-            string hash1 = PasswordHasher.GetHash(password);
-            string hash2 = PasswordHasher.GetHash(password, "nice69", 69);
-            Assert.AreNotEqual(hash1, hash2);
+            Assert.IsNotNull(PasswordHasher.GetHash(password, null, 42069));
+            Assert.AreNotEqual(PasswordHasher.GetHash(password, null, 42069), PasswordHasher.GetHash(password, "nice", 42069));
         }
 
-
+        [TestMethod]
+        public void TestCase_Init_0_3()
+        {
+            string password = "password42069";
+            Assert.IsNotNull(PasswordHasher.GetHash(password));
+            Assert.AreNotEqual(PasswordHasher.GetHash(password), PasswordHasher.GetHash(password, "nice", 42069));
+        }
     }
 }
